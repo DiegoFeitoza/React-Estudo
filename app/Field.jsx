f2522de2329444bb553a82ee8d6139dae9b2843a
Diego Feitoza <diegofeitoza@yahoo.com.br>
2019-05-06 11:53:50 -0300

    Aula 15 - Redu

--- app/Field.jsx
+++ app/Field.jsx
@@ -1,20 +1,29 @@
 import React, {Component} from 'React'
-import { connect } from 'react-redux'
 
 class Field extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			value: props.firstValue,
+		}
+		this.handleChangeInput = this.handleChangeInput.bind(this)
+	}
+
+	handleChangeInput(e){
+		this.setState({
+			...this.state,
+			value: e.target.value
+		})		
+	}
+
 	render(){
 		return(
 			<div>
-				<label style={{marginBottom: '20px', fontSize: '25px', fontWeight: 'bold',display: 'block'}}>{this.props.value}</label>
-				<input style={{background: 'transparent',outline:'none',border:'none',borderBottom:'2px solid gray',fontSize: '18px',color: 'rgba(150,150,150,0.8)',paddingLeft: '5px'}} type="text" onChange={this.handleChangeInput} value={this.props.value} />
+				<label style={{marginBottom: '20px', fontSize: '25px', fontWeight: 'bold',display: 'block'}}>{this.state.value}</label>
+				<input style={{background: 'transparent',outline:'none',border:'none',borderBottom:'2px solid gray',fontSize: '18px',color: 'rgba(150,150,150,0.8)',paddingLeft: '5px'}} type="text" onChange={this.handleChangeInput} value={this.state.value} />
 			</div>
 		)
 	}
 }
 
-function mapStateToProps(state){
-	return{
-		value: state.field.value
-	}
-}
-export default connect(mapStateToProps)(Field)
+export default Field
