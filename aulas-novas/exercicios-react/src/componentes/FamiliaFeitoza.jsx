3cb316ee2e1236b99b3ea79a041dc96b3de1e293
Feitoza, Diego <diegofeitoza@yahoo.com.br>
2019-09-04 14:20:12 -0300

    Att arquivos para as aulas

---
+++ aulas-novas/exercicios-react/src/componentes/FamiliaFeitoza.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Membro from './Membro'
+
+export default props =>
+    <div>
+        <Membro nome="Diego" sobrenome="Feitoza" />
+        <Membro nome="Larissa" sobrenome="Feitoza" />
+        <Membro nome="Danilo" sobrenome="Feitoza" />
+    </div>
---
+++ aulas-novas/exercicios-react/src/componentes/FamiliaFreitas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Membro from './Membro'
+
+export default props =>
+    <React.Fragment>
+        <Membro nome="Erika" sobrenome="Freitas" />
+        <Membro nome="Alexandre" sobrenome="Freitas" />
+        <Membro nome="Larissa" sobrenome="Freitas" />
+        <Membro nome="Luciana" sobrenome="Freitas" />
+        <Membro nome="Lorena" sobrenome="Freitas" />
+        <Membro nome="Danilo" sobrenome="Freitas" />
+    </React.Fragment>
